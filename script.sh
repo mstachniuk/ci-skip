79aa223e806404561991e29f173a3e78dd556b4b
Marcin Stachniuk <mstachniuk@gmail.com>
2020-11-13 22:02:32 +0100

    Extract script sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+
+
+echo "fail-fast ${fail-fast}"
+echo "exit-code ${exit-code}"
+echo "fail_fast ${fail_fast}"
+echo "exit_code ${exit_code}"
+echo "FAIL_FAST ${FAIL_FAST}"
+echo "EXIT_CODE ${EXIT_CODE}"
+echo "End"
