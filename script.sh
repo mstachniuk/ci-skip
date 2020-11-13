951303bca42f3c8d0ffeb286ff3eb90d19c76aa0
Marcin Stachniuk <mstachniuk@gmail.com>
2020-11-13 22:08:25 +0100

    Test inputs

--- script.sh
+++ script.sh
@@ -2,9 +2,9 @@
 
 echo "fail-fast ${fail-fast}"
 echo "exit-code ${exit-code}"
-echo "inputs.fail-fast ${{ inputs.fail-fast }}"
-echo "inputs.exit-code ${{ inputs.exit-code }}"
-echo "inputs.fail_fast ${{ inputs.fail_fast }}"
-echo "inputs.exit_code ${{ inputs.exit_code }}"
+echo "arg 0 $0"
+echo "arg 1 $1"
+echo "arg 2 $2"
+echo "arg 3 $3"
 
 echo "End"
