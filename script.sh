7263138803243223b01c3f92dae17a577af4747f
Marcin Stachniuk <mstachniuk@gmail.com>
2020-11-13 22:28:47 +0100

    Test

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 
 fail_fast=$1
 exit_code=$2
+commit_filter=$3
 
 # Based on: https://github.com/marketplace/actions/skip-based-on-commit-message
 last_commit_log=$(git log -1 --pretty=format:"%s %b")
@@ -12,18 +13,18 @@ if [[ "$is_merge_commit" -eq 1 ]]; then
 fi
 echo "last commit log: $last_commit_log"
 
-readonly local filter_count=$(echo "$last_commit_log" | fgrep -c "$COMMIT_FILTER")
-echo "number of occurence of '$COMMIT_FILTER' in '$last_commit_log': $filter_count"
+readonly local filter_count=$(echo "$last_commit_log" | fgrep -c "$commit_filter")
+echo "number of occurrences of '$commit_filter' in '$last_commit_log': $filter_count"
 
 if [[ "$filter_count" -eq 0 ]]; then
   echo "all good, continue"
 else
-  if fail_fast; then
-    echo "the last commit log \"$last_commit_log\" contains \"$COMMIT_FILTER\", stopping"
-    # TODO: How to finish workflow with success?
+  if [[ $fail_fast == 'true' ]]; then
+    echo "the last commit log \"$last_commit_log\" contains \"$commit_filter\", stopping"
+    # It's impossible to finish workflow with success, so exiting using exit-code
     exit "$exit_code"
   else
-    echo "the last commit log \"$last_commit_log\" contains \"$COMMIT_FILTER\", setting environment variables:"
+    echo "the last commit log \"$last_commit_log\" contains \"$commit_filter\", setting environment variables:"
     echo "CI_SKIP=true"
     echo "CI_SKIP_NOT=false"
     echo "CI_SKIP=true" >> $GITHUB_ENV
